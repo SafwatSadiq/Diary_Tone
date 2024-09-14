75e08f3fbeaac05323fe0562d857577b6a28468f
Safwat Sadiq <Safwatsadiq14@gmail.com>
2024-09-14 16:34:34 +0600

    Made the backend code little short

--- backend.py
+++ backend.py
@@ -8,24 +8,17 @@ analyzer = SentimentIntensityAnalyzer()
 
 def get_data():
     date = []
-    mood_pos = []
-    mood_neg = []
+    pos = []
+    neg = []
     for filename in glob.glob("diary\*.txt"):
         with open(filename, 'r') as file:
             # Reading content
             content = file.read()
             # Getting neg and pos moods value
-            pos, neg = get_mood(content)
-            mood_pos.append(pos)
-            mood_neg.append(neg)
+            score = analyzer.polarity_scores(content)
+            pos.append(score["pos"])
+            neg.append(score["neg"])
             # Got the date
             filepath = pathlib.Path(filename).name
             date.append(filepath.split('.')[0])
-    return date, mood_pos, mood_neg
-
-
-def get_mood(content):
-    score = analyzer.polarity_scores(content)
-    pos = score["pos"]
-    neg = score["neg"]
-    return pos, neg
+    return date, pos, neg
